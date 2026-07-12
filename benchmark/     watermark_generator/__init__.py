d05569a13191e44106a96f8f4028271ea6b34f74
Abdurahmanvaqifli <126266232+Abdurahmanvaqifli@users.noreply.github.com>
2026-07-13 01:32:02 +0400

    Initialize watermark generator package
    Added module docstring and imports for watermark generation.

--- benchmark/     watermark_generator/__init__.py
+++ benchmark/     watermark_generator/__init__.py
@@ -1,1 +1,24 @@
+"""
+AWSRE Benchmark Watermark Generator Package
+"""
 
+from benchmark.watermark_generator.base_generator import (
+    BaseWatermarkGenerator,
+    GeneratedWatermark,
+    WatermarkType,
+)
+
+from benchmark.watermark_generator.binary_pattern import (
+    BinaryPatternGenerator,
+    BinaryPatternType,
+    generate_binary_pattern,
+)
+
+__all__ = [
+    "BaseWatermarkGenerator",
+    "GeneratedWatermark",
+    "WatermarkType",
+    "BinaryPatternGenerator",
+    "BinaryPatternType",
+    "generate_binary_pattern",
+]
