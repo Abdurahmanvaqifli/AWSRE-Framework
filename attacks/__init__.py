2965fcb39984cec1988ae68233ac063dbaf861b2
Abdurahmanvaqifli <126266232+Abdurahmanvaqifli@users.noreply.github.com>
2026-07-13 21:04:16 +0400

    Initialize AWSRE Attack Simulation Package
    Added module docstring and imports for attack simulation.

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,1 +1,23 @@
+"""
+AWSRE Attack Simulation Package
+"""
 
+from attacks.pipeline import (
+    AttackPipeline,
+    AttackResult,
+    AttackStep,
+    SUPPORTED_ATTACKS,
+    apply_attack,
+    apply_combined_attacks,
+    normalize_attack_name,
+)
+
+__all__ = [
+    "AttackPipeline",
+    "AttackResult",
+    "AttackStep",
+    "SUPPORTED_ATTACKS",
+    "apply_attack",
+    "apply_combined_attacks",
+    "normalize_attack_name",
+]
